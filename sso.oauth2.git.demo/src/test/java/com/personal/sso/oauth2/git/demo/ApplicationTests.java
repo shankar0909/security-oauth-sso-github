48f69887695baca98aa1d0202f9e5bdbcdb1ef31
shankar09-personal <shankar09-personal@shankars-mbp-2.lan>
2020-02-23 16:40:35 -0500

    Initial commit

---
+++ sso.oauth2.git.demo/src/test/java/com/personal/sso/oauth2/git/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.sso.oauth2.git.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
